3f0ea85708d15f7ec51f80187f4f224499ab17a5
Charles Loder <charles.w.loder@gmail.com>
2019-12-08 15:08:57 -0500

    issue #6 - sequencing

--- src/sequence.js
+++ src/sequence.js
@@ -12,12 +12,11 @@ module.exports = text => {
     let accents = /[\u0590-\u05AF, \u05BD-\u05BE, \u05C0, \u05C3]/;
     return text.split(splits).map(e => {
           return e.split('').sort((a,b) => {
-            // since the str is split at consonants, the first a is always const, thus never flip
-             if(consonants.test(a) ) {return 0}
              // if a is anything except a consonant and b is a ligature, then flip
              if(!consonants.test(a) && ligature.test(b)){return 1}
              if(vowels.test(a) && dagesh.test(b)) {return 1}
              if(accents.test(a) && dagesh.test(b)) {return 1}
+             if(accents.test(a) && vowels.test(b)) {return 1}
         }).join('')
       }).join('')
 }
