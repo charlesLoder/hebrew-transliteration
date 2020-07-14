e53de946f64ab09577f538d953de369572cf46d7
Charles Loder <charles.w.loder@gmail.com>
2020-07-14 09:46:46 -0400

    shin dot now removed also

--- test/remove.test.ts
+++ test/remove.test.ts
@@ -1,9 +1,9 @@
 import { remove } from "../src/index";
 
 test("remove ONLY the cantillation", () => {
-  expect(remove("בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים")).toBe("בְּרֵאשִׁית בָּרָא אֱלֹהִים");
+  expect(remove("בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים")).toBe("בְּרֵאשִית בָּרָא אֱלֹהִים");
 });
 
 test("remove cantillation AND vowels", () => {
-  expect(remove("בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים", { removeVowels: true })).toBe("בראשׁית ברא אלהים");
+  expect(remove("בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים", { removeVowels: true })).toBe("בראשית ברא אלהים");
 });
