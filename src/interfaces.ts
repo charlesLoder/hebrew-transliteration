29ea7324506605287c637397a6488f34e80e0999
Charles Loder <cloder@classicalsubjects.com>
2021-11-04 06:47:04 -0400

    Add docs to remove options

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,9 +4,36 @@ export interface TransOptions {
   isSimple?: boolean;
 }
 
+/**
+ * Options for the `remove()` function
+ */
 export interface RemoveOptions {
+  /**
+   * an option to remove vowels
+   * @example
+   * ```ts
+   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true });
+   * // "שׂרי אשׁתך";
+   * ```
+   */
   removeVowels?: boolean;
+  /**
+   * an option to remove the shin dot (U+05C1)
+   * @example
+   * ```ts
+   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true, removeShinDot: true, removeSinDot: true });
+   * // "שרי אשתך";
+   * ```
+   */
   removeShinDot?: boolean;
+  /**
+   * an option to remove the sin dot (U+05C2)
+   * @example
+   * ```ts
+   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true, removeShinDot: true, removeSinDot: true });
+   * // "שרי אשתך";
+   * ```
+   */
   removeSinDot?: boolean;
 }
 
