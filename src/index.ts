bf71b09d5c979c88c58a52cbf7f5f629d562ed2a
Charles Loder <cloder@classicalsubjects.com>
2021-11-13 18:01:16 -0500

    Update export of index

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,6 @@
-import { TransOptions, RemoveOptions } from "./interfaces";
-import { Transliterate } from "./transliterate";
-import { Sequence } from "./sequence";
-import { Remove } from "./remove";
+import { Text } from "havarotjs";
+import { transliterate } from "./transliterate";
+import { sequence } from "./sequence";
+import { remove } from "./remove";
 
-export const transliterate = (
-  text: string,
-  { isSequenced = true, qametsQatan = false, isSimple = false }: TransOptions = {}
-) => {
-  const normalized = text.normalize("NFKD");
-  return Transliterate(normalized, { isSequenced, qametsQatan, isSimple });
-};
-
-export const remove = (
-  text: string,
-  { removeVowels = false, removeShinDot = false, removeSinDot = false }: RemoveOptions = {}
-) => {
-  const normalized = text.normalize("NFKD");
-  const removed = Remove(normalized, { removeVowels, removeShinDot, removeSinDot });
-  const noMetheg = removed.replace(/\u{05BD}/gu, "");
-  const sequenced = Sequence(noMetheg);
-  return sequenced;
-};
-
-export const sequence = (text: string) => {
-  const normalized = text.normalize("NFKD");
-  return Sequence(normalized);
-};
+export { remove, sequence, Text, transliterate };
