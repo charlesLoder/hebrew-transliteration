b4a589660ecaf5523436ec0886e468e4f50821ac
Charles Loder <cloder@classicalsubjects.com>
2021-11-27 09:51:29 -0500

    Add schema and text to be passed in

--- src/transliterate.ts
+++ src/transliterate.ts
@@ -3,33 +3,19 @@ import { sylRules, wordRules } from "./rules";
 import { mapChars } from "./mapChars";
 import { SBL, Schema } from "./interfaces";
 import { Text } from "havarotjs";
-import { SylOpts } from "havarotjs/dist/text";
 import { Word } from "havarotjs/dist/word";
 
-/**
- *
- * @param schema
- * @returns syllable options passed into havarotjs
- * @description sanitizes the SylOpts of the schema so as to not pass in undefined
- */
-const getSylOpts = (schema: Partial<SylOpts>) => {
-  const options: SylOpts = {};
-  if ("longVowels" in schema) options.longVowels = schema.longVowels;
-  if ("qametsQatan" in schema) options.qametsQatan = schema.qametsQatan;
-  if ("sqnmlvy" in schema) options.sqnmlvy = schema.sqnmlvy;
-  if ("wawShureq" in schema) options.wawShureq = schema.wawShureq;
-  return options;
-};
-
-export const transliterate = (text: string, schema?: Partial<Schema & SylOpts>) => {
-  const sbl = new SBL(schema ?? {});
-  if (!vowels.test(text)) return mapChars(text, sbl);
-  const sylOptions = getSylOpts(schema ?? {});
-  return new Text(text, sylOptions).words
+export const transliterate = (text: string | Text, schema?: Partial<Schema> | Schema) => {
+  const transSchema = schema instanceof Schema ? schema : new SBL(schema ?? {});
+  const isText = text instanceof Text;
+  // prevents Text from throwing error when no vowels
+  if (!isText && !vowels.test(text)) return mapChars(text, transSchema);
+  const newText = isText ? text : new Text(text, transSchema);
+  return newText.words
     .map((word) => {
-      let transliteration = wordRules(word, sbl);
+      let transliteration = wordRules(word, transSchema);
       if (transliteration instanceof Word) {
-        transliteration = word.syllables.map((s) => sylRules(s, sbl)).join("");
+        transliteration = word.syllables.map((s) => sylRules(s, transSchema)).join("");
       }
       return `${transliteration}${word.whiteSpaceAfter}`;
     })
