d7409188818c1f05baa1a9c4d07fc041188c129d
Charles Loder <cloder@classicalsubjects.com>
2021-11-27 15:02:36 -0500

    Add syl opts check
    - was throwing error

--- src/transliterate.ts
+++ src/transliterate.ts
@@ -4,13 +4,30 @@ import { mapChars } from "./mapChars";
 import { SBL, Schema } from "./interfaces";
 import { Text } from "havarotjs";
 import { Word } from "havarotjs/dist/word";
+import { SylOpts } from "havarotjs/dist/text";
+
+/**
+ *
+ * @param schema
+ * @returns syllable options passed into havarotjs
+ * @description sanitizes the SylOpts of the schema so as to not pass in undefined
+ */
+const getSylOpts = (schema: Partial<SylOpts>) => {
+  const options: SylOpts = {};
+  if ("longVowels" in schema) options.longVowels = schema.longVowels;
+  if ("qametsQatan" in schema) options.qametsQatan = schema.qametsQatan;
+  if ("sqnmlvy" in schema) options.sqnmlvy = schema.sqnmlvy;
+  if ("wawShureq" in schema) options.wawShureq = schema.wawShureq;
+  return options;
+};
 
 export const transliterate = (text: string | Text, schema?: Partial<Schema> | Schema) => {
   const transSchema = schema instanceof Schema ? schema : new SBL(schema ?? {});
   const isText = text instanceof Text;
   // prevents Text from throwing error when no vowels
   if (!isText && !vowels.test(text)) return mapChars(text, transSchema);
-  const newText = isText ? text : new Text(text, transSchema);
+  const sylOptions = getSylOpts(schema ?? {});
+  const newText = isText ? text : new Text(text, sylOptions);
   return newText.words
     .map((word) => {
       let transliteration = wordRules(word, transSchema);
