f37a66b4030d107e0c2ad18474d0b9b608ac01bd
Charles Loder <cloder@classicalsubjects.com>
2021-11-27 08:47:50 -0500

    Update transliterate to use schema class
    - setSchemaDefaults is now done by SBL class
    - getSylOpts takes partial and better checking

--- src/transliterate.ts
+++ src/transliterate.ts
@@ -1,27 +1,10 @@
 import { vowels } from "./sequence";
 import { sylRules, wordRules } from "./rules";
 import { mapChars } from "./mapChars";
-import { Schema } from "./interfaces";
-import { sbl } from "./sbl";
+import { SBL, Schema } from "./interfaces";
 import { Text } from "havarotjs";
 import { SylOpts } from "havarotjs/dist/text";
 import { Word } from "havarotjs/dist/word";
-
-/**
- *
- * @param userSchema
- * @param defaultSchema
- * @returns a schema with defaults added
- * @description modifies the userSchema according to the defaultSchema
- */
-const setSchemaDefaults = (userSchema: Schema, defaultSchema: Schema) => {
-  for (const key of Object.keys(defaultSchema)) {
-    if (!userSchema[key]) {
-      userSchema[key] = defaultSchema[key];
-    }
-  }
-  return userSchema;
-};
 
 /**
  *
@@ -29,24 +12,24 @@ const setSchemaDefaults = (userSchema: Schema, defaultSchema: Schema) => {
  * @returns syllable options passed into havarotjs
  * @description sanitizes the SylOpts of the schema so as to not pass in undefined
  */
-const getSylOpts = (schema: SylOpts) => {
+const getSylOpts = (schema: Partial<SylOpts>) => {
   const options: SylOpts = {};
-  if (schema.longVowels) options.longVowels = schema.longVowels;
-  if (schema.qametsQatan) options.qametsQatan = schema.qametsQatan;
-  if (schema.sqnmlvy) options.sqnmlvy = schema.sqnmlvy;
-  if (schema.wawShureq) options.wawShureq = schema.wawShureq;
+  if ("longVowels" in schema) options.longVowels = schema.longVowels;
+  if ("qametsQatan" in schema) options.qametsQatan = schema.qametsQatan;
+  if ("sqnmlvy" in schema) options.sqnmlvy = schema.sqnmlvy;
+  if ("wawShureq" in schema) options.wawShureq = schema.wawShureq;
   return options;
 };
 
-export const transliterate = (text: string, schema?: Schema & SylOpts) => {
-  const sanitizedSchema: Schema & SylOpts = !schema ? sbl : setSchemaDefaults(schema, sbl);
-  if (!vowels.test(text)) return mapChars(text, sanitizedSchema);
-  const sylOptions = getSylOpts(sanitizedSchema);
+export const transliterate = (text: string, schema?: Partial<Schema & SylOpts>) => {
+  const sbl = new SBL(schema ?? {});
+  if (!vowels.test(text)) return mapChars(text, sbl);
+  const sylOptions = getSylOpts(schema ?? {});
   return new Text(text, sylOptions).words
     .map((word) => {
-      let transliteration = wordRules(word, sanitizedSchema);
+      let transliteration = wordRules(word, sbl);
       if (transliteration instanceof Word) {
-        transliteration = word.syllables.map((s) => sylRules(s, sanitizedSchema)).join("");
+        transliteration = word.syllables.map((s) => sylRules(s, sbl)).join("");
       }
       return `${transliteration}${word.whiteSpaceAfter}`;
     })
