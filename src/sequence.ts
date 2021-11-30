9b70e937bf717296e22a3f0290bf9b3da1f1eeed
Charles Loder <cloder@classicalsubjects.com>
2021-11-29 20:23:39 -0500

    Add type annotation

--- src/sequence.ts
+++ src/sequence.ts
@@ -8,6 +8,6 @@ export const vowels = /[\u{05B0}-\u{05BD}\u{05BF}\u{05C7}]/gu;
  * @param {boolean} qametsQatan
  * @returns {string} a sequenced string of text
  */
-export const sequence = (text: string, qametsQatan = false) => {
+export const sequence = (text: string, qametsQatan: boolean = false): string => {
   return vowels.test(text) ? new Text(text, { qametsQatan }).text : text;
 };
