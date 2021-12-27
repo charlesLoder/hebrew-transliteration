ddbfeb42a3bf2e389976c6d51786399a311da5e5
Charles Loder <cloder@classicalsubjects.com>
2021-12-27 17:12:46 -0500

    Update docs

--- src/sequence.ts
+++ src/sequence.ts
@@ -3,12 +3,13 @@ import { Text } from "havarotjs";
 export const vowels = /[\u{05B0}-\u{05BD}\u{05BF}\u{05C7}]/u;
 
 /**
+ * sequences Hebrew charactes according to the [SBL Hebrew Font Manual](https://www.sbl-site.org/Fonts/SBLHebrewUserManual1.5x.pdf)
  *
  * @param text - a string of Hebrew character
  * @param qametsQatan - option to convert regular qamets characters to qamets qatan
  * @returns a sequenced string of text
  * @remarks
- * seqeuncing follows the patters of: consonant - dagesh - vowel - ta'am as defined in the {@link https://www.sbl-site.org/Fonts/SBLHebrewUserManual1.5x.pdf | SBL Hebrew Font Manual}
+ * seqeuncing follows the pattern of: consonant - dagesh - vowel - ta'am as defined in the {@link https://www.sbl-site.org/Fonts/SBLHebrewUserManual1.5x.pdf | SBL Hebrew Font Manual}
  *
  * @example
  *
