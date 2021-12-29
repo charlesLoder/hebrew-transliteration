2e48d5d0c5bc45af3a246c2966e00e2b81b412c9
Charles Loder <cloder@classicalsubjects.com>
2021-12-29 08:56:15 -0500

    Refactor remove options

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,42 +1,6 @@
 import { SylOpts } from "havarotjs/dist/text";
 
 /**
- * Options for the `remove()` function
- */
-export interface RemoveOptions {
-  /**
-   * an option to remove vowels
-   *
-   * @example
-   * ```ts
-   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true });
-   * // "שׂרי אשׁתך";
-   * ```
-   */
-  removeVowels?: boolean;
-  /**
-   * an option to remove the shin dot (U+05C1)
-   *
-   * @example
-   * ```ts
-   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true, removeShinDot: true, removeSinDot: true });
-   * // "שרי אשתך";
-   * ```
-   */
-  removeShinDot?: boolean;
-  /**
-   * an option to remove the sin dot (U+05C2)
-   *
-   * @example
-   * ```ts
-   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true, removeShinDot: true, removeSinDot: true });
-   * // "שרי אשתך";
-   * ```
-   */
-  removeSinDot?: boolean;
-}
-
-/**
  * class for defining a schema for transliteration
  */
 export class Schema implements SylOpts {
--- src/remove.ts
+++ src/remove.ts
@@ -1,9 +1,44 @@
-import { RemoveOptions } from "./interfaces";
 import { sequence } from "./sequence";
 const cantillation = /[\u{0591}-\u{05AF}\u{05BF}\u{05C0}\u{05C3}-\u{05C6}\u{05F3}\u{05F4}]/gu;
 const vowels = /[\u{05B0}-\u{05BD}\u{05BF}\u{05C7}]/gu;
 const shinDot = /\u{05C1}/gu;
 const sinDot = /\u{05C2}/gu;
+
+/**
+ * Options for the `remove()` function
+ */
+interface RemoveOptions {
+  /**
+   * an option to remove vowels
+   *
+   * @example
+   * ```ts
+   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true });
+   * // "שׂרי אשׁתך";
+   * ```
+   */
+  removeVowels?: boolean;
+  /**
+   * an option to remove the shin dot (U+05C1)
+   *
+   * @example
+   * ```ts
+   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true, removeShinDot: true, removeSinDot: true });
+   * // "שרי אשתך";
+   * ```
+   */
+  removeShinDot?: boolean;
+  /**
+   * an option to remove the sin dot (U+05C2)
+   *
+   * @example
+   * ```ts
+   * heb.remove("שָׂרַ֣י אִשְׁתְּךָ֔", { removeVowels: true, removeShinDot: true, removeSinDot: true });
+   * // "שרי אשתך";
+   * ```
+   */
+  removeSinDot?: boolean;
+}
 
 const removeItem = (text: string, item: RegExp) => text.replace(item, "");
 
