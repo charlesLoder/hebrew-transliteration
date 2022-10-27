b57195e448f4cdc5d060bff16ce4d169885b2ef6
Charles Loder <cloder@classicalsubjects.com>
2022-10-27 16:44:09 -0400

    Fix module resolver

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   testEnvironment: "node",
   testRegex: "/test/.*\\.(test|spec)?\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  rootDir: "./"
+  rootDir: "./",
+  moduleNameMapper: {
+    "havarotjs/dist/utils/regularExpressions": "<rootDir>/node_modules/havarotjs/dist/utils/regularExpressions"
+  }
 };
