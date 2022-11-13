dbfae5ef595115fa889f1464bee4f1f8a66cd210
Charles Loder <cloder@classicalsubjects.com>
2022-11-13 10:22:48 -0500

    Add basic schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -3,5 +3,6 @@ import { brillSimple } from "./brillSimple";
 import { sblAcademicSpirantization } from "./sblAcademicSpirantization";
 import { sblSimple } from "./sblSimple";
 import { michiganClaremont } from "./michiganClaremont";
+import { romaniote } from "./romaniote";
 
-export { brillAcademic, brillSimple, sblAcademicSpirantization, sblSimple, michiganClaremont };
+export { brillAcademic, brillSimple, sblAcademicSpirantization, sblSimple, michiganClaremont, romaniote };
