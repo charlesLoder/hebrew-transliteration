b408cc68eb554300416fae8308e0e8b14e651f3e
Charles Loder <cloder@classicalsubjects.com>
2023-02-03 01:24:35 -0500

    Add hiriq tests

--- test/schemas/romaniote.test.ts
+++ test/schemas/romaniote.test.ts
@@ -110,6 +110,19 @@ describe.each`
 });
 
 describe.each`
+  description                       | hebrew             | transliteration
+  ${"hiriq yod: medial"}            | ${"סִיר"}          | ${"σιρ"}
+  ${"hiriq yod: final"}             | ${"אֲנִי"}         | ${"ανη"}
+  ${"hiriq yod: plural marker"}     | ${"דְּבָרִים"}     | ${"ντεβαρείμ"}
+  ${"hiriq yod: final with maqqef"} | ${"וַֽיְהִי־כֵֽן"} | ${"βαγιεη-χεν"}
+`("Hiriq Yod:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
   description           | hebrew       | transliteration
   ${"medial tsere-yod"} | ${"אֵין"}    | ${"εν"}
   ${"final tsere-yod"}  | ${"רִגְעֵי"} | ${"ριγαί"}
