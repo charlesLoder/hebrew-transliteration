bdfd3e3b8e50dac3a7d3a8231e4fe87961c6caf1
charlesLoder <charles.w.loder@gmail.com>
2024-06-25 16:30:17 -0400

    Remove comment

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,4 @@ import { sequence } from "./sequence";
 import { remove } from "./remove";
 import { Schema } from "./schema";
 
-/**
- * I accidentally published a canary version without a tag, making it "latest".
- * Just adding this comment and creating a new version to make it latest
- */
-
 export { remove, Schema, sequence, Text, transliterate };
