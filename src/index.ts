4cf7fb979b88bbfbfe81bf1d71dcdc943e2c7d83
charlesLoder <charles.w.loder@gmail.com>
2026-06-03 23:28:43 -0400

    simplify exports

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,17 @@
-import { Text } from "havarotjs";
-import type { RemoveOptions } from "./remove";
-import { remove } from "./remove";
-import type {
-  ClusterCallback,
-  ClusterFeature,
-  HebrewFeature,
-  PassThrough,
-  SyllableCallback,
-  SyllableFeature,
-  SylOpts,
-  WordCallback,
-  WordFeature
-} from "./schema";
-import {
-  SBL,
-  Schema
-} from "./schema";
-import { sequence } from "./sequence";
-import { transliterate } from "./transliterate";
-
-export { remove, SBL, Schema, sequence, Text, transliterate };
+export { Text } from "havarotjs";
+export { remove } from "./remove";
+export type { RemoveOptions } from "./remove";
+export { SBL, Schema } from "./schema";
 export type {
   ClusterCallback,
   ClusterFeature,
   HebrewFeature,
   PassThrough,
-  RemoveOptions,
   SyllableCallback,
   SyllableFeature,
   SylOpts,
   WordCallback,
   WordFeature
-};
+} from "./schema";
+export { sequence } from "./sequence";
+export { transliterate } from "./transliterate";
